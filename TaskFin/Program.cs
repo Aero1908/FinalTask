07dc5150e08f39d5000968530c41fab6c670c0bb
Yervand <hakobyan.ea@gmail.com>
2023-03-11 22:48:09 +0300

    Final task done

--- TaskFin/Program.cs
+++ TaskFin/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "AYG" };
+string[] arr3 = { "Lesson", "Field", "Tree" };
+
+int maxLength = 3;
+
+string[] GetShortWordsArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            result[count] = array[i];
+            count++;
+        }
+
+    }
+    if (count == 0)
+    {
+        result = new string[0];
+    }
+    string[] shortArr = new string[count];
+    shortArr = result;
+    return shortArr;
+}
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("Такие элементы отсутствуют");
+    }
+    else
+    {
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i < array.Length - 1)
+                Console.Write($"{array[i]} ");
+            else Console.WriteLine($"{array[i]} ");
+        }
+
+    }
+}
+
+void PrintTask(string[] array)
+{
+    Console.WriteLine("Заданный массив:");
+    PrintArray(array);
+    string[] shortArray = GetShortWordsArray(array);
+    Console.WriteLine($"Массив с длинной элементов менее или равной {maxLength} символам:");
+    PrintArray(shortArray);
+    Console.WriteLine();
+}
+
+PrintTask(arr1);
+PrintTask(arr2);
+PrintTask(arr3);
